7cd7a4facadd2306fdd232ba689c37e7cc95489f
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2022-04-27 10:17:17 -0400

    now updates wallpaper everyday

--- Nasa_picture_of_day.py
+++ Nasa_picture_of_day.py
@@ -1,9 +1,11 @@
-
 import os
 import schedule
 import pathlib
 import subprocess
 import time
+from bs4 import BeautifulSoup
+import requests
+from datetime import date
 
 SET_WALLPAPER_OSASCRIPT = """/usr/bin/osascript<<END
 tell application "Finder"
@@ -11,19 +13,46 @@ set desktop picture to POSIX file "{}"
 end tell
 END"""
 
+PICTURE_URL_BEGINNING = "https://apod.nasa.gov/apod/ap220430"
+IMAGE_INFO_URL = "https://apod.nasa.gov/apod/"
+
+
+class SpecificDateClass:
+    day = "02"
+
+
+def get_wallpaper():
+    today = date.today()
+    curr_year = today[2:4]
+    curr_month = today[5:7]
+    curr_day = today[8:]
+
+    picture_url = PICTURE_URL_BEGINNING + curr_year + curr_month + curr_day \
+                  + ".html "
+    request = requests.get(picture_url)
+    soup = BeautifulSoup(request.text, "html.parser")
+    images = soup.find_all('img')
+    for image in images:
+        name = str(today) + '.jpg'
+        link = image['src']
+        with open(name, 'wb') as f:
+            image_info = requests.get(IMAGE_INFO_URL + link)
+            f.write(image_info.content)
+    return str(today)
+
 
 def set_wallpaper():
+    name_of_image = get_wallpaper()
     wallpaper_path = os.path.join(
-        # get file path of project
         pathlib.Path().absolute(),
-        "M104_HST_final2_1024.jpg"
+        name_of_image + ".jpg"
     )
     subprocess.Popen(SET_WALLPAPER_OSASCRIPT.format(wallpaper_path), shell=True)
 
 
 def change_wallpaper():
-    # schedule for desktop picture to change every 24 hours
-    schedule.every(10).seconds.do(set_wallpaper)
+
+    schedule.every(24).hours.do(set_wallpaper)
     while True:
         schedule.run_pending()
         time.sleep(1)
