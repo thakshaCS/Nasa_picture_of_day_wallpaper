4dca2513a3578e080d81f4217e7499c0ffd4b8e9
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2022-04-27 16:40:04 -0400

    fixed bugs and wrote comments

--- Nasa_picture_of_day.py
+++ Nasa_picture_of_day.py
@@ -13,35 +13,37 @@ set desktop picture to POSIX file "{}"
 end tell
 END"""
 
-PICTURE_URL_BEGINNING = "https://apod.nasa.gov/apod/ap220430"
+PICTURE_URL_BEGINNING = "https://apod.nasa.gov/apod/ap"
 IMAGE_INFO_URL = "https://apod.nasa.gov/apod/"
 
 
-class SpecificDateClass:
-    day = "02"
-
-
-def get_wallpaper():
-    today = date.today()
+def get_wallpaper() -> str:
+    """ Creates the Nasa picture of the day and returns the name of the image
+    that was created.
+    """
+    today = str(date.today())
     curr_year = today[2:4]
     curr_month = today[5:7]
     curr_day = today[8:]
 
     picture_url = PICTURE_URL_BEGINNING + curr_year + curr_month + curr_day \
-                  + ".html "
+                  + ".html"
+    name = str(today) + '.jpg'
     request = requests.get(picture_url)
     soup = BeautifulSoup(request.text, "html.parser")
     images = soup.find_all('img')
     for image in images:
-        name = str(today) + '.jpg'
         link = image['src']
         with open(name, 'wb') as f:
             image_info = requests.get(IMAGE_INFO_URL + link)
             f.write(image_info.content)
-    return str(today)
+    return today
 
 
 def set_wallpaper():
+    """
+    Sets wallpaper to image with the name that is returned from get_wallpaper.
+    """
     name_of_image = get_wallpaper()
     wallpaper_path = os.path.join(
         pathlib.Path().absolute(),
@@ -51,8 +53,10 @@ def set_wallpaper():
 
 
 def change_wallpaper():
-
-    schedule.every(24).hours.do(set_wallpaper)
+    """
+    Changes the wallpaper every 24 hours by calling set_wallpaper.
+    """
+    schedule.every(5).seconds.do(set_wallpaper)
     while True:
         schedule.run_pending()
         time.sleep(1)
